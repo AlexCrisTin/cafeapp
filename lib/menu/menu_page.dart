f2340d0d5f30d4ced77c19ca960b3a13314b8dde
AlexCrisTin <nholaian2222@gmail.com>
2025-09-23 15:12:06 +0700

    new

--- lib/menu/menu_page.dart
+++ lib/menu/menu_page.dart
@@ -6,7 +6,21 @@ class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Menu Page'),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.red,
+            height: 70,
+            width: double.infinity,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Menu', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
